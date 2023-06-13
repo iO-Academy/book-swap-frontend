d2851c45c159a3a9f3cfe7933c1c9acf4794af8a
norayna <rayna.bozhkova@mayden.co.uk>
2023-06-13 16:08:35 +0100

    More hardcoded story 1 stuff such as styling

--- src/Components/BookItem.js
+++ src/Components/BookItem.js
@@ -2,7 +2,7 @@ const BookItem = ({id, title, author, genre, imgSrc}) => {
     return (
         <div className="max-w-xs bg-violet-300 p-6 m-6">
             <img className="w-full" src={imgSrc}/>
-            <h2>{title}</h2>
+            <h2 className="text-xl font-semibold">{title}</h2>
             <p>{author}</p>
             <p>{genre}</p>
         </div>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,11 @@
 const Navbar = () => {
-
+    return (
+        <nav className="w-full bg-green-400 p-3">
+            <h1 className="text-2xl font-semibold">
+                Book Swap
+            </h1>
+        </nav>
+    )
 }
 
 export default Navbar
