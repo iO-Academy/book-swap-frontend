a6750117317023a02ce203de8d29741deee0cffc
norayna <rayna.bozhkova@mayden.co.uk>
2023-06-14 14:23:20 +0100

    h1 in navbar is now a link to the homepage

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom"
+
 const Navbar = () => {
     return (
         <nav className="w-full bg-green-400 p-3">
-            <h1 className="text-2xl font-semibold">
-                Book Swap
-            </h1>
+            <Link to="/">
+                <h1 className="text-2xl font-semibold">
+                    Book Swap
+                </h1>
+            </Link>
         </nav>
     )
 }
