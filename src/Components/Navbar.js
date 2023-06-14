f0b224724ec59a0aa319d695c94ad102bdfc58ed
norayna <rayna.bozhkova@mayden.co.uk>
2023-06-14 15:25:17 +0100

    Added links to the navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,13 +1,17 @@
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 
 const Navbar = () => {
     return (
-        <nav className="w-full bg-green-400 p-3">
+        <nav className="flex flex-row flex-no-wrap justify-between w-full bg-green-400 p-3">
             <Link to="/">
                 <h1 className="text-2xl font-semibold">
                     Book Swap
                 </h1>
             </Link>
+            <div className="flex flex-row gap-3">
+                <NavLink className={({isActive, isPending}) => isActive ? 'font-semibold' : 'hover:text-slate-500 transition-colors'} to="/">Available books</NavLink>
+                <NavLink className={({isActive, isPending}) => isActive ? 'font-semibold' : 'hover:text-slate-500 transition-colors'} to="/claimed">Claimed books</NavLink>
+            </div>
         </nav>
     )
 }
