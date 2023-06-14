1b6710f953a6aa248123fe80ab3551860f9cb1ae
norayna <rayna.bozhkova@mayden.co.uk>
2023-06-14 12:13:43 +0100

    Book item on homepage now links to book page

--- src/Components/BookItem.js
+++ src/Components/BookItem.js
@@ -1,11 +1,15 @@
+import { Link } from 'react-router-dom'
+
 const BookItem = ({id, title, author, genre, image}) => {
     return (
-        <div className="max-w-xs bg-violet-300 p-6 m-6">
-            <img className="w-full" src={image}/>
-            <h2 className="text-xl font-semibold">{title}</h2>
-            <p>{author}</p>
-            <p>{genre}</p>
-        </div>
+        <Link to={'/books/' + id} >
+            <div className="max-w-xs bg-violet-300 p-6 m-6">
+                <img className="w-full" src={image}/>
+                <h2 className="text-xl font-semibold">{title}</h2>
+                <p>{author}</p>
+                <p>{genre}</p>
+            </div>
+        </Link>
     )
 }
 
