f5cdffcadb60f710549ae7eb8df3c674d39c6db2
norayna <rayna.bozhkova@mayden.co.uk>
2023-06-20 17:15:30 +0100

    Improved navbar responsiveness

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,13 +2,13 @@ import { Link, NavLink } from "react-router-dom"
 
 const Navbar = () => {
     return (
-        <nav className="flex flex-row flex-no-wrap justify-between w-full bg-green-400 p-3">
+        <nav className="flex flex-col md:flex-row flex-no-wrap justify-between items-center w-full bg-green-400 p-3">
             <Link to="/">
                 <h1 className="text-2xl font-semibold">
                     Book Swap
                 </h1>
             </Link>
-            <div className="flex flex-row gap-3">
+            <div className="flex flex-row flex-wrap gap-3">
                 <NavLink className={({isActive, isPending}) => isActive ? 'font-semibold' : 'hover:text-slate-500 transition-colors'} to="/">Available books</NavLink>
                 <NavLink className={({isActive, isPending}) => isActive ? 'font-semibold' : 'hover:text-slate-500 transition-colors'} to="/claimed">Claimed books</NavLink>
             </div>
